81f432bcd368a0e96bafcda226febf73460e8ee0
faGitLab <facled@yahoo.com.ar>
2025-07-12 08:36:05 -0300

    Corrigiendo el componente logo

--- src/main/java/com/demoVaadin/vaadin1/components/Logo2DComponent.java
+++ src/main/java/com/demoVaadin/vaadin1/components/Logo2DComponent.java
@@ -5,25 +5,25 @@ import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 
-public class Logo2DAndNameComponent extends FlexLayout {
+public class Logo2DComponent extends FlexLayout {
 
-    public Logo2DAndNameComponent() {
+    public Logo2DComponent() {
         setAlignItems(Alignment.CENTER);
         getStyle().set("flex-wrap", "wrap");
 
-        addClassName("logo-name-wrapper");
+        addClassName("logo"); // bloque principal
         setWidthFull();
         setHeight("80px");
 
-        Image logoImg = new Image("images/Icono-sinFondo-ChatGPT.png", "Neticware logo");
-        logoImg.addClassName("logo-animated");
+        Image logoImg = new Image("images/Icono-sinFondo-ChatGPT.png", "Neticware icon");
+        logoImg.addClassNames("logo__icon", "logo__icon--2d", "logo__icon--animated");
         logoImg.setHeight("40px");
 
         Anchor logoLink = new Anchor("#hero", logoImg);
-        logoLink.addClassName("logo-link");
+        logoLink.addClassName("logo__link");
 
         Span nombre = new Span("neticware");
-        nombre.addClassName("logo-text");
+        nombre.addClassName("logo__text");
 
         add(logoLink, nombre);
     }
--- src/main/java/com/demoVaadin/vaadin1/sections/Footer.java
+++ src/main/java/com/demoVaadin/vaadin1/sections/Footer.java
@@ -1,10 +1,6 @@
 package com.demoVaadin.vaadin1.sections;
 
-import com.demoVaadin.vaadin1.components.Logo2DAndNameComponent;
-
-import com.demoVaadin.vaadin1.components.PruebaComponente;
-
-import com.vaadin.flow.component.html.H2;
+import com.demoVaadin.vaadin1.components.Logo2DComponent;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
@@ -15,7 +11,7 @@ public class Footer extends VerticalLayout {
         setAlignItems(Alignment.CENTER);
         setSpacing(true);
 
-        add(new Logo2DAndNameComponent());
+        add(new Logo2DComponent());
         add(new Paragraph("@NETICWARE 2025 - Todos los derechos Reservados"));
 
 
